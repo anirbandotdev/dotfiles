acf265347b56096a5f5410782ac13d68534243cf
anirbandotdev <anirbaniitian@gmail.com>
2026-06-05 17:51:01 +0530

    Add lsp config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,7 @@
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.tabstop = 4     
-vim.o.softtabstop = 4  
+vim.o.softtabstop = 4 
 vim.o.shiftwidth = 4  
 vim.o.expandtab = true
 vim.g.mapleader = " "
@@ -9,15 +9,24 @@ vim.g.mapleader = " "
 vim.cmd.colorscheme "catppuccin"
 
 vim.pack.add({ 
-
 	"https://github.com/catppuccin/nvim",
 	"https://github.com/tree-sitter/tree-sitter",
 	"https://github.com/wakatime/vim-wakatime",
-    "https://github.com/IogaMaster/neocord"
+  "https://github.com/IogaMaster/neocord",
+  "https://github.com/nvim-telescope/telescope.nvim",
+  "https://github.com/nvim-lua/plenary.nvim",
+  "https://github.com/mason-org/mason.nvim",
  })
 
-
- vim.keymap.set('n', '<leader>e', vim.cmd.Ex, { desc = 'Open File Explorer' })
+vim.keymap.set('n', '<leader>e', vim.cmd.Ex, { desc = 'Open File Explorer' })
 vim.keymap.set("i", "jk", "<ESC>")
+vim.keymap.set('i', '<c-space>', function()
+  vim.lsp.completion.get()
+end)
+
 
 require("plugins.presence")
+require("plugins.telescope")
+require("plugins.lsp")
+require("mason").setup()
+
