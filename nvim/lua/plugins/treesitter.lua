78caef8cba5403d34d7018b1c6bd59ae4334831c
anirbandotdev <anirbaniitian@gmail.com>
2026-06-10 17:40:53 +0530

    Remove folding

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -18,7 +18,4 @@ vim.api.nvim_create_autocmd("FileType", {
 	end,
 })
 
-vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
-vim.wo[0][0].foldmethod = "expr"
-
 vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
